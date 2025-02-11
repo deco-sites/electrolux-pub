2da2505730ec3ef06123531bbb6e5a757c5931da
L. Sacci <167649583+aka-sacci-ccr@users.noreply.github.com>
2025-02-11 19:34:20 -0300

    Update elux-components-app.ts

--- apps/local/elux-components-app.ts
+++ apps/local/elux-components-app.ts
@@ -1,2 +1,2 @@
-export { default } from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@d051fca8162daa4ccba499fa3263b850bb817a1e/mod.ts";
-export * from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@d051fca8162daa4ccba499fa3263b850bb817a1e/mod.ts";
+export { default } from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@bc5cbda31e34da315a1e64dc0e119f6814d31a48/mod.ts";
+export * from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@bc5cbda31e34da315a1e64dc0e119f6814d31a48/mod.ts";
