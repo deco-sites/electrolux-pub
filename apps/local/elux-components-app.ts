3809ba7f2673e1843778cb7d0edead7451fdea7c
decobot <capy@deco.cx>
2025-02-12 16:21:34 -0300

    sync main

--- apps/local/elux-components-app.ts
+++ apps/local/elux-components-app.ts
@@ -1,2 +1,2 @@
-export { default } from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@e8075bc62b92b9b9baa94f611f5a08adf345707c/mod.ts";
-export * from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@e8075bc62b92b9b9baa94f611f5a08adf345707c/mod.ts";
+export { default } from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@e0627d5d28da124352598956ba54fde334fad0c1/mod.ts";
+export * from "https://cdn.jsdelivr.net/gh/aka-sacci-ccr/elux-components-app@e0627d5d28da124352598956ba54fde334fad0c1/mod.ts";
